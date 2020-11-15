2ae76531cfac7ff1795e287719ac37794deaa087
Daniel Bradford <danielbradford@hotmail.co.uk>
2020-11-15 13:27:13 +0000

    Image upload reenabled for admin view

--- products/admin.py
+++ products/admin.py
@@ -3,6 +3,38 @@ from .models import Product, Category, Color, Size
 
 # Register your models here.
 
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'name',
+        'category',
+        'price',
+        'image',
+    )
+
+    ordering = ('name',)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+
+class colorAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+class sizeAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
 admin.site.register(Product)
 admin.site.register(Category)
 admin.site.register(Color)
--- products/models.py
+++ products/models.py
@@ -50,8 +50,8 @@ class Product(models.Model):
     color = models.ForeignKey('Color', null=True, blank=True, on_delete=models.SET_NULL)
     size = models.ForeignKey('Size', null=True, blank=True, on_delete=models.SET_NULL)
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    image_url = models.URLField(max_length=1024, null=True, blank=True),
-    image = models.ImageField(null=True, blank=True),
-
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+    
     def __str__(self):
         return self.name
