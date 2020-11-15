fab08937c917aea59b57feacc4551989b4599aef
Daniel Bradford <danielbradford@hotmail.co.uk>
2020-11-15 12:53:47 +0000

    Admin customized and models added

--- products/admin.py
+++ products/admin.py
@@ -1,7 +1,41 @@
 from django.contrib import admin
-from .models import Product, Category
+from .models import Product, Category, Color, Size
 
 # Register your models here.
 
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'name',
+        'category',
+        'price',
+        'image',
+    )
+
+    ordering = ('name',)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+
+class colorAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+class sizeAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
 admin.site.register(Product)
 admin.site.register(Category)
+admin.site.register(Color)
+admin.site.register(Size)
--- products/models.py
+++ products/models.py
@@ -2,9 +2,38 @@ from django.db import models
 
 # Create your models here.
 
+
 class Category(models.Model):
+
+    class Meta:
+        verbose_name_plural = 'Categories'
     name = models.CharField(max_length=254)
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+
+class Color(models.Model):
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+class Size(models.Model):
+    name = models.CharField(max_length=254)
+    measurement = models.CharField(max_length=254, null=True, blank=True)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    image = models.ImageField(null=True, blank=True)
 
     def __str__(self):
         return self.name
@@ -18,10 +47,11 @@ class Product(models.Model):
     sku = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=254)
     description = models.TextField()
+    color = models.ForeignKey('Color', null=True, blank=True, on_delete=models.SET_NULL)
+    size = models.ForeignKey('Size', null=True, blank=True, on_delete=models.SET_NULL)
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
-    image_url = models.URLField(max_length=1024, null=True, blank=True)
-    image = models.ImageField(null=True, blank=True)
+    image_url = models.URLField(max_length=1024, null=True, blank=True),
+    image = models.ImageField(null=True, blank=True),
 
     def __str__(self):
         return self.name
