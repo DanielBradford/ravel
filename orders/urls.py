02a15a1afc8b59906f4e6e9ab8a8d101642c080d
Daniel Bradford <danielbradford@hotmail.co.uk>
2020-12-06 11:08:15 +0000

    added add to order functionality

--- orders/urls.py
+++ orders/urls.py
@@ -1,8 +1,9 @@
-
+from django.contrib import admin
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('', views.orders, name='orders'),
+    path('add/<item_id>/', views.add_to_order, name='add_to_order'),
 
 ]
--- orders/views.py
+++ orders/views.py
@@ -5,4 +5,24 @@ from django.shortcuts import render, redirect
 
 def orders(request):
     """ A view to renders the orders page"""
-    return render(request, 'orders/orders.html')
+    return render(request, 'orders/orderForm.html')
+
+
+def add_to_order(request, item_id):
+                
+    """ Add a quantity of the specified product to the order """
+
+    quantity = int(request.POST.get('quantity'))
+    color = request.POST.get('color')
+    size = request.POST.get('size')
+    redirect_url = request.POST.get('redirect_url')
+    order = request.session.get('order', {})
+
+    if item_id in list(order.keys()):
+        order[item_id] += quantity
+    else:
+        order[item_id] = quantity, color, size
+
+    request.session['order'] = order
+    print(request.session['order'])
+    return redirect(redirect_url)
