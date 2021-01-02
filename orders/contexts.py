c6fdaa14f9cac4dcb669e18f0520df0fd3934458
Daniel Bradford <danielbradford@hotmail.co.uk>
2021-01-02 18:51:43 +0000

    Color and size correctly displayed in order form. For loop altered in contexts.py

--- orders/contexts.py
+++ orders/contexts.py
@@ -13,41 +13,56 @@ def order_contents(request):
 
     for item_id, quantity in order.items():
         product = get_object_or_404(Product, pk=item_id)
-        colorChoice = str(quantity[1])
+        colorChoice = quantity[1]
         print(colorChoice)
-        
-
-        # colorCost = ''
         for color in colors:
-            # print("This is the color choice" item.name)
             if colorChoice == color.name:
-                colorCost = decimal.Decimal(color.cost)
-                print(colorCost)
+                print("This is the color choice", colorChoice, color.cost)
+                colorCost = color.cost
+                sizeChoice = quantity[2]
+                sizes = Size.objects.all()
+                for size in sizes:
+                    if sizeChoice == size.name:
+                        sizeCost = decimal.Decimal(size.cost)
+                        extras = decimal.Decimal(sizeCost + colorCost)
+                        print("The extras are", extras)
+                        total += (product.price + extras) * decimal.Decimal(quantity[0])
+                        order_items.append({
+                            'item_id': item_id,
+                            'quantity': quantity[0],
+                            'color': colorChoice,
+                            'colorCost': colorCost,
+                            'sizeCost': sizeCost,
+                            'extras': extras,
+                            'size': sizeChoice,
+                            'product': product,
+                            'this_total': (product.price + extras) * decimal.Decimal(quantity[0])
+                        })
             else:
-                colorCost = decimal.Decimal(0)
-        sizeChoice = quantity[2]
-        sizes = Size.objects.all()
-        for size in sizes:
-            if sizeChoice == size.name:
-                sizeCost = decimal.Decimal(size.cost)
-            else:
-                sizeCost = decimal.Decimal(0)
-        sizeCost = sizeCost
-        colorCost = colorCost
-        extras = (sizeCost + colorCost)
-        print("The extras are", extras)
-        total += (product.price + extras) * decimal.Decimal(quantity[0])
-        order_items.append({
-            'item_id': item_id,
-            'quantity': quantity[0],
-            'color': colorChoice,
-            'colorCost': colorCost,
-            'sizeCost': sizeCost,
-            'extras': extras,
-            'size': sizeChoice,
-            'product': product,
-            'this_total': (product.price + extras) * decimal.Decimal(quantity[0])
-        })
+                colorCost = 0
+        # print("TEST IF wrong", colorCost)
+        # sizeChoice = quantity[2]
+        # sizes = Size.objects.all()
+        # for size in sizes:
+        #     if sizeChoice == size.name:
+        #         sizeCost = decimal.Decimal(size.cost)
+            # else:
+            #     sizeCost = 88
+    
+        # extras = decimal.Decimal(sizeCost + colorCost)
+        # print("The extras are", extras)
+        # total += (product.price + extras) * decimal.Decimal(quantity[0])
+        # order_items.append({
+        #     'item_id': item_id,
+        #     'quantity': quantity[0],
+        #     'color': colorChoice,
+        #     'colorCost': colorCost,
+        #     'sizeCost': sizeCost,
+        #     'extras': extras,
+        #     'size': sizeChoice,
+        #     'product': product,
+        #     'this_total': (product.price + extras) * decimal.Decimal(quantity[0])
+        # })
                 
     if total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = settings.STANDARD_DELIVERY
--- orders/views.py
+++ orders/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, redirect
 from django import template
+import string
+import random
+
 # from .models import Product, Color, Size
 
 # register = template.Library()
@@ -20,6 +23,8 @@ from django import template
 #     'size': size
 # }
 
+def id_generator(size=6, chars=string.ascii_uppercase + string.digits):
+    return ''.join(random.choice(chars) for _ in range(size))
 
 def orders(request):
     """ A view to render the orders page"""
@@ -29,13 +34,13 @@ def orders(request):
 def add_to_order(request, item_id):
                 
     """ Add a quantity of the specified product to the order """
-
+    orderID = id_generator()
     quantity = float(request.POST.get('quantity'))
     color = str(request.POST.get('color'))
     size = str(request.POST.get('size'))
     redirect_url = request.POST.get('redirect_url')
     order = request.session.get('order', {})
-    order[item_id] = quantity, color, size
+    order[item_id] = quantity, color, size, orderID
     request.session['order'] = order
     print(order)
     print(len(order))
