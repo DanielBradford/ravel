38a3fed9bb31bfb3d754598ccf770a50e4872483
Daniel Bradford <danielbradford@hotmail.co.uk>
2021-01-10 13:13:10 +0000

    Checkout form, css and requirements updated

--- checkout/views.py
+++ checkout/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.contrib import messages
 
-# Create your views here.
+from .forms import OrderForm
+
+
+def checkout(request):
+    order = request.session.get('order', {})
+    if not order:
+        messages.error(request, "There's nothing in your order at the moment")
+        return redirect(reverse('products'))
+
+    order_form = OrderForm()
+    template = 'checkout/checkout.html'
+    context = { 
+        'order_form': order_form,
+    }
+
+    return render(request, template, context)
--- ravel/settings.py
+++ ravel/settings.py
@@ -49,6 +49,9 @@ INSTALLED_APPS = [
     'products',
     'orders',
     'checkout',
+
+    #other
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -62,6 +65,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'ravel.urls'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 TEMPLATES = [
     {
@@ -80,6 +85,10 @@ TEMPLATES = [
                 'django.contrib.messages.context_processors.messages',
                 'orders.contexts.order_contents',
             ],
+             'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
+            ]
         },
     },
 ]
