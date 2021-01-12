4f2c1c56b167e976164472e51ce0f5775fbb4bcb
Daniel Bradford <danielbradford@hotmail.co.uk>
2021-01-12 13:43:02 +0000

    Stripe Keys added to environment and views to link functionality of checkout

--- checkout/views.py
+++ checkout/views.py
@@ -1,21 +1,43 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
+from django.conf import settings
 
 from .forms import OrderForm
+from orders.contexts import order_contents
 
+import stripe
 
 def checkout(request):
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+
+
+
     order = request.session.get('order', {})
     if not order:
         messages.error(request, "There's nothing in your order at the moment")
         return redirect(reverse('products'))
+    
+    current_order = order_contents(request)
+    total = current_order['grand_total']
+    stripe_total = round(total * 100)
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY,
+    )
 
     order_form = OrderForm()
+
+    if not stripe_public_key:
+        messages.warning(request, 'Stripe public key is missing. \
+            Did you forget to set in your environment?')
+            
     template = 'checkout/checkout.html'
     context = { 
         'order_form': order_form,
-        'stripe_public_key': 'pk_test_51HaMScH8WfdiqwToKUMAMbY3QaTTOTHWwFHvbq5iq6CNKFMmeJ6oQqoiknQCIwddRu7TVRrGK4abMQmTQW9YO4yp00vAXCT6uz',
-        'client_secret': 'test_client_secret'
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
     }
 
     return render(request, template, context)
