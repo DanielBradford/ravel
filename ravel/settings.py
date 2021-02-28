58ad34cd16a8ebc9b4fa114c1c9fc6960f77a60e
Daniel Bradford <danielbradford@hotmail.co.uk>
2021-02-28 13:48:20 +0000

    managing deployment

--- ravel/settings.py
+++ ravel/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = '5wthha!jh$4u8rds#%o2g#*88rtm*cq4_%&vdjj$pawczadk*+'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [ 'ravel-online.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -123,14 +123,18 @@ SITE_ID = 1
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    }
+else:
+    DATABASES = {
+        'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        }
+ 
 }
-
-
 
 
 
