64b490e06df3ba660c9c9e7d373ba8790a136bf8
Daniel Bradford <danielbradford@hotmail.co.uk>
2021-02-28 13:57:03 +0000

    Removed secret key and updated debug

--- ravel/settings.py
+++ ravel/settings.py
@@ -25,10 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '5wthha!jh$4u8rds#%o2g#*88rtm*cq4_%&vdjj$pawczadk*+'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = [ 'ravel-online.herokuapp.com', 'localhost']
 
