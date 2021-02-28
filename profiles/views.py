bd2471bc4f5b514485e65df0e6ad822c71ec8e9d
Daniel Bradford <danielbradford@hotmail.co.uk>
2021-02-28 12:36:56 +0000

    Updated order history table  for logged in user

--- profiles/views.py
+++ profiles/views.py
@@ -34,14 +34,17 @@ def order_history(request, order_number):
     order = get_object_or_404(Order, order_number=order_number)
 
     messages.info(request, (
-        f'This is a past confirmation for order number {order_number}. '
+        f'This is a past confirmation for order number {order_number}.'
         'A confirmation email was sent on the order date.'
     ))
 
     template = 'checkout/checkout_success.html'
+    total_spend = sum(item.price for item in order)
+
     context = {
         'order': order,
         'from_profile': True,
+        'total_spend': total_spend,
     }
 
     return render(request, template, context)
