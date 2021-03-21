a161dcadfe141fb2ff5b01adff2b89fc5fa130cc
Daniel Bradford <danielbradford@hotmail.co.uk>
2021-03-21 14:14:40 +0000

    Contact page re-introduced for user story with simplet mailto method

--- home/views.py
+++ home/views.py
@@ -1,6 +1,28 @@
-from django.shortcuts import render
+from django.core.mail import send_mail, BadHeaderError
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render, redirect
+from .forms import ContactForm
 
-# Create your views here.
+def contactView(request):
+    if request.method == 'GET':
+        form = ContactForm()
+    else:
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            subject = form.cleaned_data['subject']
+            from_email = form.cleaned_data['from_email']
+            message = form.cleaned_data['message']
+            try:
+                send_mail(subject, message, from_email, ['''customer.service.
+                ravel.online@gmail.com'''])
+            except BadHeaderError:
+                return HttpResponse('Invalid header found.')
+            return redirect('success')
+    return render(request, "email.html", {'form': form})
+
+
+def successView(request):
+    return HttpResponse('Success! Thank you for your message.')
 
 
 def index(request):
@@ -11,3 +33,5 @@ def index(request):
 def contact(request):
     """ A view to return the contact page"""
     return render(request, 'home/contact.html')
+
+
