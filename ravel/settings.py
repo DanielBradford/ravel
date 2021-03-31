00c66314bbf561d6c4747640f4621c618c6cd42f
Daniel Bradford <danielbradford@hotmail.co.uk>
2021-03-31 19:56:26 +0000

    Attempt to solve missing media files

--- ravel/settings.py
+++ ravel/settings.py
@@ -189,9 +189,12 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_ROOT = os.path.join(BASE_DIR, "mediafiles")
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
+)
+MEDIA_DIRS = (
+    os.path.join(BASE_DIR, 'media'),
 )
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
