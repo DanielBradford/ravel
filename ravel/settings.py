9c7c0f7e716be81cc2d197afc8f2c23ee4ff8779
Daniel Bradford <danielbradford@hotmail.co.uk>
2021-03-31 20:03:34 +0000

    Working media files issue

--- ravel/settings.py
+++ ravel/settings.py
@@ -189,12 +189,9 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, "mediafiles")
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
-)
-MEDIA_DIRS = (
-    os.path.join(BASE_DIR, 'media'),
 )
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
